3af7228756d720c8c21e60e6d408d6bdc2ab4598
Bharat8211 <62749870+Bharat8211@users.noreply.github.com>
2021-07-19 20:56:44 +0530

    Add files via upload

---
+++ static/build/precache-manifest.5e64dc11be842e62f000737dad35d523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a10889bb57d98f0712feb01572c61c",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "85711e6182d14a0b4d77",
+    "url": "/TodoList/static/css/main.b4606167.chunk.css"
+  },
+  {
+    "revision": "81fcfe3d10e658e96b53",
+    "url": "/TodoList/static/js/2.ce8e2d4a.chunk.js"
+  },
+  {
+    "revision": "85711e6182d14a0b4d77",
+    "url": "/TodoList/static/js/main.a5e1695a.chunk.js"
+  },
+  {
+    "revision": "4aff89a528f793eada99",
+    "url": "/TodoList/static/js/runtime~main.df469424.js"
+  },
+  {
+    "revision": "65aab60bc90908f2cc32c76e07bba3bd",
+    "url": "/TodoList/static/media/homework.65aab60b.png"
+  }
+]);
